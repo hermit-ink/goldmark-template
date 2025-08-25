174f593a9f874c722a585b1462e16feffa33420b
Raoul Millais <raoul@raoulmillais.com>
2025-08-25 10:51:54 +0100

    refactor: tidy up parsers

--- parser.go
+++ parser.go
@@ -7,34 +7,24 @@ import (
 
 // NewParser creates a parser with our custom link parser replacing the default
 func NewParser() parser.Parser {
-	// Get default parsers
-	blockParsers := parser.DefaultBlockParsers()
-	inlineParsers := parser.DefaultInlineParsers()
-	paragraphTransformers := parser.DefaultParagraphTransformers()
-	
-	// Remove the default link parser and add ours
-	filteredInlineParsers := make([]util.PrioritizedValue, 0, len(inlineParsers))
-	for _, pv := range inlineParsers {
-		// Check if this is goldmark's default link parser by type assertion
-		// The default link parser is an unexported *linkParser type
-		if _, isLinkParser := pv.Value.(interface{ Trigger() []byte }); isLinkParser {
-			// Check if triggers match ['!', '[', ']'] 
-			triggers := pv.Value.(interface{ Trigger() []byte }).Trigger()
-			if len(triggers) == 3 && triggers[0] == '!' && triggers[1] == '[' && triggers[2] == ']' {
-				// This is the default link parser, skip it
+	ip := parser.DefaultInlineParsers()
+
+	parsers := make([]util.PrioritizedValue, 0, len(ip))
+	for _, pv := range ip {
+		if lp, ok := pv.Value.(interface{ Trigger() []byte }); ok {
+			t := lp.Trigger()
+			if len(t) == 3 && t[0] == '!' && t[1] == '[' && t[2] == ']' {
+				// Looks like a duck, talks like a duck
+				parsers = append(parsers, util.Prioritized(NewLinkParser(), 200))
 				continue
 			}
 		}
-		filteredInlineParsers = append(filteredInlineParsers, pv)
+		parsers = append(parsers, pv)
 	}
-	
-	// Add our templated link parser at the same priority (200)
-	filteredInlineParsers = append(filteredInlineParsers, 
-		util.Prioritized(NewLinkParser(), 200))
-	
+
 	return parser.NewParser(
-		parser.WithBlockParsers(blockParsers...),
-		parser.WithInlineParsers(filteredInlineParsers...),
-		parser.WithParagraphTransformers(paragraphTransformers...),
+		parser.WithBlockParsers(parser.DefaultBlockParsers()...),
+		parser.WithInlineParsers(parsers...),
+		parser.WithParagraphTransformers(parser.DefaultParagraphTransformers()...),
 	)
 }
